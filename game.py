439cbc5740d8fd7326eb6660bda6fda33c833f73
shoshlavosh <shoshlavosh@gmail.com>
2022-01-14 04:37:54 +0000

    updated with user name and error if outside the given numbers

--- game.py
+++ game.py
@@ -1,5 +1,4 @@
 """A number guessing game"""
-#didn't use the HB file download this time
 
 import random
 
@@ -7,7 +6,9 @@ answer = random.randint(1,100)
 
 print("Hello! Welcome to the Number Guessing Game.")
 print()
-print("Your task is to guess a randomly chosen number between 1 and 100.")
+name = input("Please enter your name. > ")
+print()
+print(f"Hello, {name}! Your task is to guess a randomly chosen number between 1 and 100.")
 print()
 
 def guessing_game():
@@ -19,7 +20,11 @@ def guessing_game():
 
     while guess != answer:
 
-        if guess < answer:
+        if guess > 100 or guess < 1:
+            print("Please choose a number between 1 and 100.")
+            guess = int(input("Guess a number. > "))
+
+        elif guess < answer:
             guesses += 1
             print("Too low! Try again.")
             guess = int(input("Guess a number. > "))
@@ -30,6 +35,6 @@ def guessing_game():
             guess = int(input("Guess a number. > "))
 
 
-    print(f"Correct! You guessed the answer in {guesses} tries. You win!")
+    print(f"Correct! Great job, {name}, you guessed the answer in {guesses} tries. You win!")
 
 guessing_game()
